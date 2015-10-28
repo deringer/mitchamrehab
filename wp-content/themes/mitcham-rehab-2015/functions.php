49d4f6f6084920371c8824900f495a94e43f0017
Michael Dyrynda <michael@iatstuti.net>
2015-10-28 21:10:05 +1030

    updated styles

--- wp-content/themes/mitcham-rehab-2015/functions.php
+++ wp-content/themes/mitcham-rehab-2015/functions.php
@@ -10,5 +10,7 @@ add_action('wp_head', 'mitcham_rehab_wp_head');
 
 function mitcham_rehab_wp_head()
 {
-    print '<link href="http://fonts.googleapis.com/css?family=Vollkorn" rel="stylesheet" type="text/css">';
+    print '<link href="http://fonts.googleapis.com/css?family=Vollkorn" rel="stylesheet" type="text/css">' . PHP_EOL;
+    print '<script src="https://use.typekit.net/vls2los.js"></script>' . PHP_EOL;
+    print '<script>try{Typekit.load({ async: true });}catch(e){}</script>' . PHP_EOL;
 }
